ddccc0dd0412e76ca33cceb86a774d6b393a0f1e
parthupman <parthupman@gmail.com>
2023-12-30 17:29:15 +0530

    signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        unique: true,
         required: true,
+        unique: true,
     },
     email:{
         type: String,

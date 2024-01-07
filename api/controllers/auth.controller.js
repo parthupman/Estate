f9a1f99168d147a2ceb4868498727b17505c995c
parthupman <parthupman@gmail.com>
2024-01-07 12:15:21 +0530

    signiin api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import  jwt  from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body; 
@@ -13,3 +15,18 @@ export const signup = async (req, res, next) => {
     }
     
 }                                   
+
+export const signin = async (req,res,next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if(!validUser) return next(errorHandler('User not found', 404));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler('Invalid password', 401));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' }); 
+        const {password:pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    } catch (error) {
+        next(error); 
+    }
+}

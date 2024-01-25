d2905de212bff01d23543e5509a75b643a50e9c9
parthupman <parthupman@gmail.com>
2024-01-25 14:34:51 +0530

    Finishing ui of create listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import PrivateRoutes from './components/PrivateRoutes'
+import CreateListing from './pages/CreateListing'
 
 export default function App() {
   return <BrowserRouter> 
@@ -19,6 +20,7 @@ export default function App() {
     <Route element={<PrivateRoutes/>}>
 
     <Route path="/profile" element={<Profile/>}/>
+    <Route path="/create-listing" element={<CreateListing/>}/>
     </Route>
   </Routes>
   </BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,6 +9,7 @@ import {
 
 import { app } from "../firebase";
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
     const fileRef = useRef(null)
@@ -106,6 +107,9 @@ export default function Profile() {
             <input type="text" placeholder="email" className="border p-3 rounded-lg" defaultValue={currentUser.email} id="email" onChange={handleChange}/>
             <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" onChange={handleChange}/>
             <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading?'Loading...':'Update'}</button>
+            <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+                     Create Listing
+            </Link>
           </form>
           <div className="flex justify-between mt-3">
             <span onClick={handleDeleteUser} className="text-yellow-700 cursor-pointer">Delete Account</span>

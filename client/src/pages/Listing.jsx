e0ec7c94f3b0c431d60fd0a9e14e2f70189b9cd7
parthupman <parthupman@gmail.com>
2024-02-12 22:00:02 +0530

    completing search and listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import {Swiper, SwiperSlide} from "swiper/react"
-import SwiperCore from 'swiper'
-import {Navigation} from 'swiper/modules'
-import { useSelector } from "react-redux"
-import 'swiper/css/bundle'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
 import {
   FaBath,
   FaBed,
@@ -14,45 +14,44 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
-import Contact from "../components/Contact"
+import Contact from '../components/Contact';
 
+// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
-function Listing() {
+export default function Listing() {
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
+  const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
 
-    SwiperCore.use([Navigation])
-    const [listing,setListing] = useState(null)
-    const [loading,setLoading] = useState(false)
-    const [error,setError] = useState(false)
-    const [copied, setCopied] = useState(false);
-    const [contact, setContact] = useState(false)
-    const params = useParams()
-    const {currentUser} = useSelector((state) => state.user)
-    useEffect(()=>{
-        const fetchListing = async () => {
-                try {
-                setLoading(true)
-                const res = await fetch(`/api/listing/get/${params.listingId}`)
-                const data = await res.json()
-                if(data.success === false){
-                    setError(true)
-                    setLoading(false)
-                    return
-                }
-                setListing(data)
-                setLoading(false)
-                setError(false)
-                } catch (error) {
-                    setError(true)
-                    setLoading(false)
-                }
-               
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
         }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
 
-        fetchListing()
-        },[params.listingId]
-    )
-    return (  
-     <main>
+  return (
+    <main>
       {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
       {error && (
         <p className='text-center my-7 text-2xl'>Something went wrong!</p>
@@ -107,7 +106,7 @@ function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice}
+                  ${+listing.regularPrice - +listing.discountPrice} OFF
                 </p>
               )}
             </div>
@@ -137,15 +136,18 @@ function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            {currentUser && listing.userRef !== currentUser._id && !contact &&(
-              <button onClick={()=>setContact(true)} className="bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-95">Contact Landloard</button>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
             )}
-            {contact && <Contact listing={listing}/>}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
     </main>
-    )
+  );
 }
-
-export default Listing

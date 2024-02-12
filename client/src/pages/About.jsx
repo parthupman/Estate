315e2a55290516dc5fc6e1a4c039cebe829f70dc
parthupman <parthupman@gmail.com>
2024-02-13 00:12:56 +0530

    completing application

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,12 @@
-function About() {
-    return (
-        <div>
-           About 
-        </div>
-    )
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Magic Estate</h1>
+      <p className='mb-4 text-slate-700'>Magic Estate is not just another real estate agency; we're your partners in finding your dream home or selling your property hassle-free. With our innovative approach and dedicated team, we redefine the real estate experience.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is simple yet profound: to turn your real estate dreams into reality. Whether you're searching for your perfect abode or seeking to sell your property swiftly and lucratively, Magic Estate is your trusted ally throughout the journey.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team comprises seasoned professionals with an intimate knowledge of the real estate landscape. We're not just agents; we're your guides, advocates, and confidants, committed to delivering unparalleled service tailored to your needs. We believe that every property transaction should be a magical experience, and we're here to make it happen for you.</p>
+    </div>
+  )
 }
-
-export default About
